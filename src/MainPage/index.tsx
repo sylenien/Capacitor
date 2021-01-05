ca9142c3912f81c385a70915675714ac682ecaf5
sylenien <sylenien@gmail.com>
2021-01-05 23:14:08 +0300

    handle file loading

--- src/MainPage/index.tsx
+++ src/MainPage/index.tsx
@@ -1,13 +1,19 @@
-import React, { useContext } from 'react'
+import React, { useContext, useState } from 'react'
 import styled from 'styled-components'
 import { FilePicker } from 'evergreen-ui'
 import { colors, ThemeContext } from 'const/theme'
 import { ComponentWithTheme } from 'const/types'
 
-const MainPageContainer = styled.div`
+const MainPageContainer = styled.div<ComponentWithTheme>`
   display: flex;
   flex-direction: column;
   align-items: center;
+
+  & > .customInput > button {
+    background-color: ${({ isDarkTheme }) => colors(isDarkTheme).backgroundAccent};
+    background-image: none;
+    color: ${({ isDarkTheme }) => colors(isDarkTheme).text}!important;
+  }
 `
 const Greeting = styled.div`
   font-size: 16px;
@@ -19,21 +25,67 @@ const Tip = styled.div<ComponentWithTheme>`
   color: ${({ isDarkTheme }) => colors(isDarkTheme).accent};
 `
 
+interface ScenarioStats {
+  name?: string
+  score?: string
+}
+
+const hint =
+  '<Your steam folder>\\steamapps\\common\\FPSAimTrainer\\FPSAimTrainer\\stats'
+
 const MainPage: React.FC = () => {
   const { isDarkTheme } = useContext(ThemeContext)
+  const [filesProcessed, setProcessedValue] = useState(0)
+  const [allFiles, setAllFiles] = useState(0)
+
+  const readCsv = (files: FileList) => {
+    console.log(files)
+
+    const scenarioCount = files.length
+    setAllFiles(scenarioCount)
+    setProcessedValue(0)
+
+    const promises: Promise<ScenarioStats>[] = []
+
+    Array.from(files).forEach((file, index) => {
+      const filePromise = new Promise<ScenarioStats>((resolve) => {
+        const reader = new FileReader()
+        reader.readAsText((file as unknown) as Blob)
+        reader.onload = () => {
+          if (reader.result !== null) {
+            const resultData = reader.result as string
+            const scenarioStats = {
+              name: file.name.match(/(.*).-.Challenge/i)![1],
+              score: resultData.match(/Score:,(\d*)/i)![1],
+              date: file.name.match(/Challenge.-.(.*).Stats/i)![1],
+            }
+            resolve(scenarioStats)
+            setProcessedValue(index + 1)
+          }
+          resolve({ name: 'null', score: 'null' })
+        }
+      })
+
+      promises.push(filePromise)
+    })
+
+    Promise.all(promises).then((scenarioStats) => {
+      console.log(scenarioStats)
+    })
+  }
+
   return (
-    <MainPageContainer>
+    <MainPageContainer isDarkTheme={isDarkTheme}>
       <Greeting>Upload your statistics to begin</Greeting>
       <FilePicker
         multiple
-        width={350}
-        placeholder="Select all scenario files"
-        onChange={(files) => console.log(files)}
+        width={250}
+        placeholder="0 files selected"
+        onChange={readCsv}
+        className="customInput"
       />
       <Tip isDarkTheme={isDarkTheme}>
-        {
-          '<Your steam folder>\\steamapps\\common\\FPSAimTrainer\\FPSAimTrainer\\stats'
-        }
+        {allFiles === 0 ? hint : `${filesProcessed}/${allFiles}`}
       </Tip>
     </MainPageContainer>
   )
--- src/const/theme.ts
+++ src/const/theme.ts
@@ -3,14 +3,14 @@ import React from 'react'
 const theme = {
   light: {
     background: '#f4eeff',
-    backgroundDarker: '#dcd6f7',
+    backgroundAccent: '#dcd6f7',
     accent: '#a6b1e1',
     text: '#424874',
   },
   dark: {
     background: '#424874',
     text: '#f4eeff',
-    backgroundDarker: '#a6b1e1',
+    backgroundAccent: '#a6b1e1',
     accent: '#dcd6f7',
   },
 }
